798d2c1ed6833018d5900105627cde2d630e07c3
Richard Kojedzinszky <richard@kojedz.in>
2025-09-25 13:52:23 +0200

    fix: prevent empty config on yq failure
    If `yq` fails during YAML to JSON conversion, the output file was still
    created but left empty. On subsequent container starts, this empty config
    file would be present, preventing the conversion from being retried.

--- assets/entrypoint.d/60-yaml-kea-config.sh
+++ assets/entrypoint.d/60-yaml-kea-config.sh
@@ -5,5 +5,6 @@
 
 if [ ! -f "${KEA_CONFIG}" -a -f "${KEA_CONFIG}.yaml" ]; then
     echo "[=] Converting ${KEA_CONFIG}.yaml to ${KEA_CONFIG}"
-    yq --output-format json "${KEA_CONFIG}.yaml" > "${KEA_CONFIG}"
+    yq --output-format json "${KEA_CONFIG}.yaml" > "${KEA_CONFIG}.json"
+    mv "${KEA_CONFIG}.json" "${KEA_CONFIG}"
 fi
